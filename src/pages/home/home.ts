f0667e7f443ff8f3c512719c6a86ae8670d01be9
Benjamin Delacombaz <benjamin.delacombaz@cpnv.ch>
2018-05-07 14:13:21 +0200

    Add home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { User } from '../../models/user';
 import { Product } from '../../models/product';
 import { Supplier } from '../../models/supplier';
 
@@ -11,10 +10,10 @@ import { Supplier } from '../../models/supplier';
 export class HomePage {
 
   product: Product
-  suppliers : Array<Supplier>
+  suppliers : Array<Supplier> = []
 
   constructor(public navCtrl: NavController) {
     this.suppliers = [new Supplier('Gégé', 'Roulat', '+41798522407', 'Impasse des Burtins 5', 'ButoxxDev'), new Supplier('Bernard', 'Marcel', '+41798522507', 'Impasse des clampins 5', 'NicoDev')]
-    this.product = new Product('Carotte', 19.50, 'kg', 6, '', this.suppliers)
+    this.product = new Product('Carotte', 19.50, 'kg', 6, 'carots.jpg', this.suppliers)
   }
 }
