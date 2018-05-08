216def1c93ab7b855c498dbaf1b5ea6d5695339b
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-05-08 08:22:34 +0200

    Add product details

--- src/models/product.ts
+++ src/models/product.ts
@@ -8,7 +8,7 @@ export class Product {
   imageFileName: string
   suppliers: Array<Supplier>
 
-  constructor(productName: string, price: number, unit: string, stock: number, imageFileName: string, suppliers: Array<Supplier>) {
+  constructor(productName: string, price: number, unit: string, stock: number, imageFileName: string, suppliers: Array<Supplier> = []) {
     this.productName = productName
     this.price = price
     this.unit = unit
