e3db0af7e68d5fd00b318934049beea33635afe8
Benjamin Delacombaz <benjamin.delacombaz@cpnv.ch>
2018-05-14 14:31:19 +0200

    Add form group

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Product } from '../../models/product';
+import { Validators, FormBuilder, FormGroup, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'page-product',
@@ -9,8 +10,14 @@ import { Product } from '../../models/product';
 export class ProductPage {
 
   product: Product
+  productForm: FormGroup
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
     this.product = navParams.get('product')
+
+    this.productForm = this.formBuilder.group({
+      price: [''],
+      unit: ['']
+    });
   }
 }
